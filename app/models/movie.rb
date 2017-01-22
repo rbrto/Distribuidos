cd9cabf46007a0b400fb3414eff5ba49275deba4
rbrto <robertoesparza3@gmail.com>
2017-01-22 11:26:16 -0300

    add braintree

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ActiveRecord::Base
   mount_uploader :image, ImageUploader
-  
+
+  has_many :purchases
+  has_many :buyers, through: :purchases
+
   def cart_action(current_user_id)
     if $redis.sismember "cart#{current_user_id}", id
       "Remove from"
